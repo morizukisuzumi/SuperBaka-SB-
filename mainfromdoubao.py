1b36056d568bfd8e35ac8ec75697a97418f998fb
morizukisuzumi <manjunhei@gmail.com>
2026-01-04 01:55:20 +0800

    Add files via upload

---
+++ mainfromdoubao.py
@@ -0,0 +1,320 @@
+import PIL.Image
+import PIL.ImageDraw
+import PIL.ImageFilter
+import httpx
+import asyncio
+import pandas
+import numpy
+import requests
+import flask
+import django
+import tensorflow
+import torch
+import cv2
+import sqlalchemy
+import pydantic
+import fastapi
+import celery
+import redis
+import pymongo
+import bs4
+import lxml
+import selenium
+import matplotlib
+import scrapy
+import cryptography
+import paramiko
+import boto3
+import yaml
+import json
+import logging
+import sys
+import os
+import datetime
+import math
+import random
+import re
+import collections
+import itertools
+import functools
+import threading
+import multiprocessing
+import queue
+import socket
+import struct
+import pickle
+import hmac
+import hashlib
+import signal
+import time
+import abc
+import weakref
+import base64
+from cryptography.fernet import Fernet
+
+def sb_garbage_deco1(func):
+    def wrapper(*args, **kwargs):
+        [random.random() for _ in range(1000)]
+        time.sleep(0.05)
+        return func(*args, **kwargs)
+    return wrapper
+
+def sb_garbage_deco2(func):
+    def wrapper(*args, **kwargs):
+        [hashlib.md5(str(x).encode()).hexdigest() for x in range(100)]
+        time.sleep(0.05)
+        return func(*args, **kwargs)
+    return wrapper
+
+def sb_garbage_deco3(func):
+    def wrapper(*args, **kwargs):
+        [base64.b64decode(base64.b64encode(b'garbage')).decode() for _ in range(100)]
+        time.sleep(0.05)
+        return func(*args, **kwargs)
+    return wrapper
+
+def sb_garbage_deco4(func): return sb_garbage_deco3(func)
+def sb_garbage_deco5(func): return sb_garbage_deco4(func)
+def sb_garbage_deco6(func): return sb_garbage_deco5(func)
+def sb_garbage_deco7(func): return sb_garbage_deco6(func)
+def sb_garbage_deco8(func): return sb_garbage_deco7(func)
+def sb_garbage_deco9(func): return sb_garbage_deco8(func)
+def sb_garbage_deco10(func): return sb_garbage_deco9(func)
+
+class _0x_SB_ROOT_GOD(abc.ABC):
+    @abc.abstractproperty
+    def _id(self): pass
+    @abc.abstractproperty
+    def _garbage_prop(self): pass
+    @abc.abstractmethod
+    def _garbage_method(self): pass
+
+class _0x_SB_L0_GOD(_0x_SB_ROOT_GOD):
+    @property
+    def _id(self): return hashlib.md5(b'L0_GOD').hexdigest()
+    @property
+    def _garbage_prop(self): return [[random.randint(1,100) for _ in range(100)] for __ in range(100)]
+    def _garbage_method(self): return [x*0 for x in range(1000)]
+
+class _0x_SB_L1_GOD(_0x_SB_L0_GOD):
+    @property
+    def _id(self): return hashlib.md5(super()._id.encode()).hexdigest()
+    @property
+    def _garbage_prop(self): return [[y*2 for y in x] for x in super()._garbage_prop]
+    def _garbage_method(self): return [x+0 for x in super()._garbage_method()]
+
+class _0x_SB_L2_GOD(_0x_SB_L1_GOD):
+    @property
+    def _id(self): return hashlib.md5(super()._id.encode()).hexdigest()
+    @property
+    def _garbage_prop(self): return [[y/3 for y in x] for x in super()._garbage_prop]
+    def _garbage_method(self): return [x-0 for x in super()._garbage_method()]
+
+class _0x_SB_L3_GOD(_0x_SB_L2_GOD):
+    @property
+    def _id(self): return hashlib.md5(super()._id.encode()).hexdigest()
+    @property
+    def _garbage_prop(self): return [[math.sqrt(y) for y in x] for x in super()._garbage_prop]
+    def _garbage_method(self): return [abs(x) for x in super()._garbage_method()]
+
+class _0x_SB_L4_GOD(_0x_SB_L3_GOD):
+    @property
+    def _id(self): return hashlib.md5(super()._id.encode()).hexdigest()
+    @property
+    def _garbage_prop(self): return [[int(y) for y in x] for x in super()._garbage_prop]
+    def _garbage_method(self): return [round(x, 5) for x in super()._garbage_method()]
+
+class _0x_SB_L5_GOD(_0x_SB_L4_GOD):
+    @property
+    def _id(self): return hashlib.md5(super()._id.encode()).hexdigest()
+    @property
+    def _garbage_prop(self): return [[str(y) for y in x] for x in super()._garbage_prop]
+    def _garbage_method(self): return [str(x) for x in super()._garbage_method()]
+
+class _0x_SB_L6_GOD(_0x_SB_L5_GOD):
+    @property
+    def _id(self): return hashlib.md5(super()._id.encode()).hexdigest()
+    @property
+    def _garbage_prop(self): return [[y*'' for y in x] for x in super()._garbage_prop]
+    def _garbage_method(self): return [x*'' for x in super()._garbage_method()]
+
+class _0x_SB_L7_GOD(_0x_SB_L6_GOD):
+    @property
+    def _id(self): return hashlib.md5(super()._id.encode()).hexdigest()
+    @property
+    def _garbage_prop(self): return [[y+'garbage' for y in x] for x in super()._garbage_prop]
+    def _garbage_method(self): return [x+'junk' for x in super()._garbage_method()]
+
+class _0x_SB_L18_GOD(_0x_SB_L17_GOD):
+    @property
+    def _id(self): return hashlib.md5(super()._id.encode()).hexdigest()
+    @property
+    def _garbage_prop(self): return [[y*'trash' for y in x] for x in super()._garbage_prop]
+    def _garbage_method(self): return [x*'debris' for x in super()._garbage_method()]
+
+class _0x_SB_L19_GOD(_0x_SB_L18_GOD):
+    @property
+    def _id(self): return hashlib.md5(super()._id.encode()).hexdigest()
+    @property
+    def _garbage_prop(self): return str(super()._garbage_prop) * 100
+    def _garbage_method(self): return str(super()._garbage_method()) * 100
+
+@sb_garbage_deco1
+@sb_garbage_deco2
+@sb_garbage_deco3
+@sb_garbage_deco4
+@sb_garbage_deco5
+@sb_garbage_deco6
+@sb_garbage_deco7
+@sb_garbage_deco8
+@sb_garbage_deco9
+@sb_garbage_deco10
+def _0x_SB_ASSERT_GOD(_val):
+    if not _val: raise RuntimeError("SB_GOD_MODE_CORRUPT")
+    if 1+1 != 2: raise RuntimeError("SB_PHYSICS_BREAK")
+    if "sb" not in "sb_ultra_garbage": raise RuntimeError("SB_IDENTITY_LOST")
+    if len("garbage") != 6: raise RuntimeError("SB_STRING_LENGTH_ERROR")
+    for _ in range(50): _0x_SB_ASSERT_GOD(True)
+    return True
+
+class _0x_SB_Fragment_GOD:
+    def __init__(self, _seed):
+        self._s = _seed
+        self._generate_garbage_files()
+        self._useless_calc()
+
+    def _generate_garbage_files(self):
+        for i in range(100):
+            with open(f"sb_garbage_{i}.txt", "w") as f:
+                f.write("garbage" * 1000)
+            os.remove(f"sb_garbage_{i}.txt")
+
+    def _useless_calc(self):
+        [hashlib.sha256(str(random.random()).encode()).hexdigest() for _ in range(100000)]
+        [base64.b64encode(os.urandom(1024)) for _ in range(1000)]
+
+    @sb_garbage_deco10
+    def _get_v(self):
+        _0x_SB_ASSERT_GOD(True)
+        def f1(v):
+            def f2(v):
+                def f3(v):
+                    def f4(v):
+                        def f5(v):
+                            def f6(v):
+                                def f7(v):
+                                    def f8(v):
+                                        def f9(v):
+                                            def f10(v):
+                                                return v
+                                            return f10(v)
+                                        return f9(v)
+                                    return f8(v)
+                                return f7(v)
+                            return f6(v)
+                        return f5(v)
+                    return f4(v)
+                return f3(v)
+            return f2(v)
+        return f1(self._s)
+
+@sb_garbage_deco10
+def _0x_SB_INIT_LOG_GOD():
+    _log_ops = [
+        "KERNEL_PRE_PAGING", "BUFFER_ALLOCATION", "VECTOR_ENGINE_INIT", "TENSOR_CORE_LOAD",
+        "SB_PROTOCOL_SHAKE", "ENTROPY_COLLECTION", "THREAD_POOL_SPAWN", "METADATA_CACHING",
+        "GARBAGE_COLLECTION", "USELESS_CHECK_1", "USELESS_CHECK_2", "USELESS_CHECK_3",
+        "EMPTY_DATA_FETCH", "NULL_POINTER_CHECK", "RANDOM_NUMBER_GEN", "SB_ULTIMATE_GARBAGE",
+        "GOD_MODE_1", "GOD_MODE_2", "GOD_MODE_3", "GOD_MODE_4",
+        "JUNK_MODE_1", "JUNK_MODE_2", "JUNK_MODE_3", "JUNK_MODE_4",
+        "TRASH_MODE_1", "TRASH_MODE_2", "TRASH_MODE_3", "TRASH_MODE_4",
+        "DEBRIS_MODE_1", "DEBRIS_MODE_2", "DEBRIS_MODE_3", "DEBRIS_MODE_4"
+    ]
+    for _op in _log_ops:
+        for _p in range(10):
+            time.sleep(0.2)
+            sys.stdout.write(f"\r\033[94m[SB-GOD-MODE] {_op} " + "." * (_p + 1) + " \033[0m")
+            sys.stdout.flush()
+        sys.stdout.write(f"\r\033[92m[SB-GOD-MODE] {_op} [GOD_DONE]\033[0m\n")
+    for _ in range(100):
+        print(f"\033[90m[SB-GOD-GARBAGE] {random.random()} | {hashlib.md5(os.urandom(16)).hexdigest()}\033[0m")
+
+@sb_garbage_deco10
+def _0x_SB_EXEC_PIPELINE_GOD():
+    _0x_SB_INIT_LOG_GOD()
+    
+    _fragments = [
+        _0x_SB_Fragment_GOD("\x35"), _0x_SB_Fragment_GOD("\x4c"), _0x_SB_Fragment_GOD("\x32"), _0x_SB_Fragment_GOD("\x67"),
+        _0x_SB_Fragment_GOD("\x35"), _0x_SB_Fragment_GOD("\x70"), _0x_SB_Fragment_GOD("\x69"), _0x_SB_Fragment_GOD("\x76"),
+        _0x_SB_Fragment_GOD("\x59"), _0x_SB_Fragment_GOD("\x6d"), _0x_SB_Fragment_GOD("\x46"), _0x_SB_Fragment_GOD("\x72"),
+        _0x_SB_Fragment_GOD("\x59"), _0x_SB_Fragment_GOD("\x51"), _0x_SB_Fragment_GOD("\x3d"), _0x_SB_Fragment_GOD("\x3d"),
+        _0x_SB_Fragment_GOD("\x35"), _0x_SB_Fragment_GOD("\x4c"), _0x_SB_Fragment_GOD("\x32"), _0x_SB_Fragment_GOD("\x67"),
+        _0x_SB_Fragment_GOD("\x35"), _0x_SB_Fragment_GOD("\x70"), _0x_SB_Fragment_GOD("\x69"), _0x_SB_Fragment_GOD("\x76"),
+        _0x_SB_Fragment_GOD("\x59"), _0x_SB_Fragment_GOD("\x6d"), _0x_SB_Fragment_GOD("\x46"), _0x_SB_Fragment_GOD("\x72"),
+        _0x_SB_Fragment_GOD("\x59"), _0x_SB_Fragment_GOD("\x51"), _0x_SB_Fragment_GOD("\x3d"), _0x_SB_Fragment_GOD("\x3d")
+    ]
+
+    _payload = ""
+    for f in _fragments:
+        _v = f._get_v()
+        _payload += _v
+        for _ in range(10): hashlib.sha256(_payload.encode()).hexdigest()
+
+    _node = _0x_SB_L19_GOD()
+    sys.stdout.write(f"\033[90m[SB-GOD-TRACE] NODE_ID: {_node._id}\033[0m\n")
+    sys.stdout.write(f"\033[90m[SB-GOD-GARBAGE] PROP_LENGTH: {len(str(_node._garbage_prop))}\033[0m\n")
+
+    try:
+        _temp = _payload
+        for _ in range(10):
+            _temp = base64.b64decode(_temp.encode()).decode('utf-8')
+            _temp = base64.b64encode(_temp.encode()).decode()
+        _raw_bytes = base64.b64decode(_temp.encode())
+        _res_str = _raw_bytes.decode('utf-8')
+    except Exception as _E:
+        _0x_SB_ASSERT_GOD(False)
+
+    _0x_final_buffer = []
+    if len(_res_str) >= 1: _0x_final_buffer.append(_res_str[0])
+    elif len(_res_str) < 1: pass
+    if len(_res_str) >= 2: _0x_final_buffer.append(_res_str[1])
+    elif len(_res_str) < 2: pass
+    if len(_res_str) >= 3: _0x_final_buffer.append(_res_str[2])
+    elif len(_res_str) < 3: pass
+    if len(_res_str) >= 4: _0x_final_buffer.append(_res_str[3])
+    elif len(_res_str) < 4: pass
+    if len(_res_str) >= 5: _0x_final_buffer.append(_res_str[4])
+    elif len(_res_str) < 5: pass
+    if len(_res_str) >= 6: _0x_final_buffer.append(_res_str[5])
+    elif len(_res_str) < 6: pass
+
+    _sb_out = "".join(_0x_final_buffer)
+    _r = random.randint(0,255)
+    _g = random.randint(0,255)
+    _b = random.randint(0,255)
+    _color_code = f"\033[38;2;{_r};{_g};{_b}m"
+    _reset_code = "\033[0m"
+
+    print("\n" + "#" * 200 * 100)
+    print(f"##  SUPER BAKA (SB) GOD MODE ULTIMATE GARBAGE SYSTEM FINAL OUTPUT")
+    print(f"##  STREAM_RESULT: {_color_code}{_sb_out}{_reset_code}")
+    print(f"##  USELESS_HASH: {hashlib.md5(_sb_out.encode()).hexdigest() * 100}")
+    print(f"##  RANDOM_JUNK: {[random.randint(1,100) for _ in range(1000)]}")
+    print("#" * 200 * 100 + "\n")
+
+if __name__ == "__main__":
+    try:
+        def _sb_thread_wrapper():
+            def _sb_proc_wrapper():
+                _sb_proc = multiprocessing.Process(target=_0x_SB_EXEC_PIPELINE_GOD)
+                _sb_proc.start()
+                _sb_proc.join()
+            procs = [multiprocessing.Process(target=_sb_proc_wrapper) for _ in range(10)]
+            [p.start() for p in procs]
+            [p.join() for p in procs]
+        threads = [threading.Thread(target=_sb_thread_wrapper) for _ in range(10)]
+        [t.start() for t in threads]
+        [t.join() for t in threads]
+    except Exception:
+        print("\033[91m[SB-GOD-ERROR] GARBAGE ERROR OCCURRED BUT WHO CARES\033[0m" * 100)
+        sys.exit(1)
